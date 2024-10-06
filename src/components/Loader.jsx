681bf78a1fcc604bf2a0de6e851e7b0d0ddfb8a3
Vikrant Yadav <vikrantmani45@gmail.com>
2024-10-06 17:54:42 +0530

    loader update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ function Loader({ load }) {
     if (load) {
         return (
             <div className=' flex flex-col items-center p-3 fixed top-[50%] left-[50%] -translate-x-2/4 -translate-y-2/4 w-full'>
-                <p className='text-white text-lg mb-2'>Loading Masterpiece...</p>
+                <p className='text-white text-lg mb-2'>Loading Vikrant's Portfolio...</p>
                 <div className="loader w-2/5"></div>
             </div>
         )
